5c6f762411340b52993d791b1df14a744139dc8f
TheSil <sildak@gmail.com>
2020-12-11 16:17:17 +0100

    added basic commands to readme + switched random vs random back to autoplay

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 Experimenting with AI ideas...
 
-TODO: 
+TODO in chess backend: 
  - threefold repetition
+
+To play with the repository, you just need to have Python 3 installed. Download this repository and go to its root folder. 
+
+Install python pre-requisities:
+
+`python -m pip install -r requirements.txt`
       
+Try running test suite:
+
+`python -m unittest discover`
+
+You can try running playing versus random AI:
+
+`python PlayerVersusRandomAi.py`
+
+Or you can try running random vs random:
+
+`python RandomAiVersusRandomAi.py`
--- RandomAiVersusRandomAi.py
+++ RandomAiVersusRandomAi.py
@@ -6,61 +6,43 @@ from chess_backend.ChessGame import ChessGame
 
 def main(do_print=True):
 
-    game_idx = 0
-    min_moves = 999
-    while True:
-        rules = ChessRulesDefault()
-        game = ChessGame(rules)
-        game.start_new()
+    rules = ChessRulesDefault()
+    game = ChessGame(rules)
+    game.start_new()
 
-        white = RandomAiController()
-        black = RandomAiController()
+    white = RandomAiController()
+    black = RandomAiController()
 
-        idx = 1
-        while game.state == game.RUNNING:
+    idx = 1
+    while game.state == game.RUNNING:
 
-            legal_moves = game.actions
-            if game.rules.state.turn == PlayerEnum.white:
-                selected = white.select_action(game.rules.state, legal_moves)
-            else:
-                selected = black.select_action(game.rules.state, legal_moves)
+        legal_moves = game.actions
+        if game.rules.state.turn == PlayerEnum.white:
+            selected = white.select_action(game.rules.state, legal_moves)
+        else:
+            selected = black.select_action(game.rules.state, legal_moves)
 
-            action = legal_moves[selected]
-            game.move(selected)
-            if do_print:
-                print(f"{idx}. move chosen: {action.move_from} -> {action.move_to}")
-                print(game.rules.state)
-
-            idx += 1
-
+        action = legal_moves[selected]
+        game.move(selected)
         if do_print:
-            if game.state == game.WHITE_WON:
-                print(f"Game finished, white won")
-            elif game.state == game.BLACK_WON:
-                print(f"Game finished, black won")
-            elif game.state == game.STALEMATE:
-                print(f"Game finished, stalemate")
-            elif game.state == game.DRAW:
-                print(f"Game finished, draw")
-
-        if idx < min_moves:
-            min_moves = idx
-            print(f"new minimal game found: {min_moves} moves")
+            print(f"{idx}. move chosen: {action.move_from} -> {action.move_to}")
             print(game.rules.state)
-            if game.state == game.WHITE_WON:
-                print(f"Game finished, white won")
-            elif game.state == game.BLACK_WON:
-                print(f"Game finished, black won")
-            elif game.state == game.STALEMATE:
-                print(f"Game finished, stalemate")
-            elif game.state == game.DRAW:
-                print(f"Game finished, draw")
-            print("")
 
-        game_idx += 1
+        idx += 1
+
+    if do_print:
+        if game.state == game.WHITE_WON:
+            print(f"Game finished, white won")
+        elif game.state == game.BLACK_WON:
+            print(f"Game finished, black won")
+        elif game.state == game.STALEMATE:
+            print(f"Game finished, stalemate")
+        elif game.state == game.DRAW:
+            print(f"Game finished, draw")
+
 
 if __name__ == '__main__':
-    main(do_print=False)
+    main()
 
     #import cProfile
     #cProfile.run('main(do_print=False)', sort="tottime")
